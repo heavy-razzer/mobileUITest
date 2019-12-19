3fc3e0f22158b02e9ae4e64ea3549bb324c71402
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-19 16:50:44 +0200

    ConfirmDialog page

---
+++ src/test/java/screens/ConfirmDialog.java
@@ -0,0 +1,45 @@
+package screens;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import test.BaseTest;
+
+public class ConfirmDialog extends BasePage {
+
+    private String TAG = "ConfirmPhoneDialog";
+
+    public ConfirmDialog() {
+        PageFactory.initElements(new AppiumFieldDecorator(BaseTest.driver), this);
+    }
+
+    @AndroidFindBy(id = "confirmDialogTitleTv")
+    private WebElement titleText;
+
+    @AndroidFindBy(id = "confirmDialogYesBtn")
+    private WebElement yesButton;
+
+    @AndroidFindBy(id = "confirmDialogNoBtn")
+    private WebElement noButton;
+
+    @Step("Wait for confirm dialog opened")
+    public ConfirmDialog waitForOpening() {
+        waitFor(titleText, "Dialog title", TAG);
+        return this;
+    }
+
+    @Step("Tap 'Yes' button")
+    public ConfirmDialog tapYesButton() {
+        click(yesButton, "Yes button", TAG);
+        return this;
+    }
+
+    @Step("Tap 'No' button")
+    public ConfirmDialog tapNoButton() {
+        click(noButton, "No button", TAG);
+        return this;
+    }
+
+}
