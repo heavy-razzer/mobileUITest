7b9ffca9a160d78a0a061a055148424aae1febf8
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-19 13:51:15 +0200

    Colours for text highlighting

---
+++ src/test/java/objects/Colours.java
@@ -0,0 +1,20 @@
+package objects;
+
+public enum Colours {
+    DEFAULT("\u001B[0m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    BLUE("\u001B[34m"),
+    CYAN("\u001B[36m"),
+    PURPLE("\u001B[35m");
+
+    private String colour;
+
+    Colours(String color) {
+        this.colour = color;
+    }
+
+    public String getValue() {
+        return colour;
+    }
+}
