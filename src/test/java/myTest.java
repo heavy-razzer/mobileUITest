48af67a139c9de5d817685f8091f5aeee7826687
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-19 16:51:41 +0200

    Add phone operations

--- src/test/java/myTest.java
+++ src/test/java/myTest.java
@@ -7,6 +7,16 @@ public class myTest extends BaseTest {
     @Test
     public void sampleTest() {
         System.out.println("xxxx");
+
+        loginPage
+                .waitForOpening()
+                .typePhoneNumber("123")
+                .tapGetCodeButton();
+
+        confirmDialog
+                .waitForOpening()
+                .tapYesButton();
+
         DriverSleep.sleep(3000);
     }
 }
