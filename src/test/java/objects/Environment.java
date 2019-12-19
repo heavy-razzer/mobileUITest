70312a4de7f021ad9ce4e29df5adf775dac03dd4
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-19 15:40:42 +0200

    Environment types list

--- src/test/java/objects/Environment.java
+++ src/test/java/objects/Environment.java
@@ -4,5 +4,4 @@ public class Environment {
 
     public static String local = MSG.ENV_LOCAL.toString();
     public static String browserStack = MSG.ENV_BS.toString();
-    public static String saucelabs = MSG.ENV_SL.toString();
 }
