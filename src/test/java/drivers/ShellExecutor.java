5f6c844bca4e9005d8f1ef641f0b730ee7a12771
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-19 15:39:02 +0200

    Class for executing shell commands

---
+++ src/test/java/drivers/ShellExecutor.java
@@ -0,0 +1,22 @@
+package drivers;
+
+
+import java.io.IOException;
+
+class ShellExecutor {
+
+    void executeShell(String[] cmd) {
+        try {
+            final Process p = new ProcessBuilder(cmd)
+                    .redirectErrorStream(true)
+                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                    .start();
+            p.destroy();
+            if (p.isAlive()) {
+                p.destroyForcibly();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
