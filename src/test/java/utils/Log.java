2cddd565f9dbd88599e2beed3e6d4fb167f43b84
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-19 13:50:53 +0200

    Class with Log messages

---
+++ src/test/java/utils/Log.java
@@ -0,0 +1,27 @@
+package utils;
+
+import objects.Colours;
+import org.junit.Assert;
+
+public class Log {
+
+    public static void log(Object text, Object... args) {
+        System.out.println(Colours.GREEN.getValue() + "[TEST]:  " + Colours.DEFAULT.getValue()
+                + String.format(text.toString(), args));
+    }
+
+    public static void errLog(Object text, Object... args) {
+        String msg = String.format(text.toString(), args);
+        System.out.println(Colours.RED.getValue() + "[ERROR]: " + Colours.DEFAULT.getValue() + msg);
+        Assert.fail(msg);
+    }
+
+    public static void sysLog(Object text, Object... args) {
+        System.out.println(Colours.BLUE.getValue() + "[SYSTEM]: " + Colours.DEFAULT.getValue()
+                + String.format(text.toString(), args));
+    }
+
+    public static void print(Object text, Colours colour) {
+        System.out.println(colour.getValue() + text.toString() + Colours.DEFAULT.getValue());
+    }
+}
