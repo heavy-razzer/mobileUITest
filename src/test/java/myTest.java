a7e943b9a075faa74905f55297fffd4e510e3a0e
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-20 15:38:02 +0200

    Sample test (no sms code, to Zon eonboarding)

--- src/test/java/myTest.java
+++ src/test/java/myTest.java
@@ -1,3 +1,4 @@
+import drivers.DriverSleep;
 import org.junit.Test;
 import test.BaseTest;
 
@@ -6,17 +7,66 @@ public class myTest extends BaseTest {
     @Test
     public void sampleTest() {
 
+        DriverSleep.sleep(9000);
+
         loginPage
                 .waitForOpening()
-                .typePhoneNumber("56146994")
+                .typePhoneNumber("6467879865")
                 .tapGetCodeButton();
 
         confirmDialog
                 .waitForOpening()
                 .tapYesButton();
 
+        verificationPage
+                .waitForOpening();
+
+        DriverSleep.sleep(15000);
+
+        emailPage
+                .waitForOpening()
+                .typeEmailAddress("voitest3@email5.net")
+                .tapLetsGoButton();
+
+        confirmDialog
+                .waitForOpening()
+                .tapYesButton();
+
+        confirmOnboardingPage
+                .waitForOpening()
+                .waitForConfirmButtonBecomeEnabled()
+                .tapConfirmButton();
+
+        enableLocationPage
+                .waitForOpening()
+                .tapEnableLocationButton()
+                .acceptLocationDialog();
+
+        homePage
+                .skipZoneOnboarding()
+                .waitForOpening()
+                .openMenu();
+
+
         //String code = OsUtils.getSMSCode();
         //System.out.println("Code is :" + code);
+
+        //homePage.waitForOpening().openMenu();
+
+        /*
+        emailPage
+                .waitForOpening()
+                .typeEmailAddress("voitest@email5.net")
+                .tapLetsGoButton();
+
+        confirmOnboardingPage
+                .waitForOpening()
+                .waitForConfirmButtonBecomeEnabled()
+                .tapConfirmButton();
+
+         */
+
+        DriverSleep.sleep(5000);
 
     }
 }
