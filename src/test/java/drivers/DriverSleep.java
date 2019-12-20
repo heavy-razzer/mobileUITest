614d0a502db92f5246b8aa84ce6d3cb23cb00855
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-20 17:31:26 +0200

    Remove unneeded function

--- src/test/java/drivers/DriverSleep.java
+++ src/test/java/drivers/DriverSleep.java
@@ -1,35 +1,19 @@
 package drivers;
 
-import io.qameta.allure.Step;
-import objects.Colours;
 import utils.Log;
 
-import static objects.DateFormats.SHORT_TIME;
 import static objects.MSG.ERR_SLEEP_FAILED;
-import static utils.Log.log;
-import static utils.OsUtils.timeStamp;
 
 /*
 Safe pause while test execution
  */
-public class DriverSleep {
+class DriverSleep {
 
-    public static void sleep(int time) {
+    static void sleep(int time) {
         try {
             Thread.sleep(time);
         } catch (InterruptedException e) {
             Log.errLog(ERR_SLEEP_FAILED);
         }
-    }
-
-    @Step("Make pause for '{seconds}' seconds")
-    public static void sleep(int seconds, String tag) {
-        log(Colours.BLUE.getValue() + timeStamp(SHORT_TIME) + Colours.DEFAULT.getValue()
-                + " - "
-                + tag
-                + ": Pause for '"
-                + Colours.PURPLE.getValue() + seconds + Colours.DEFAULT.getValue()
-                + "' seconds");
-        sleep(seconds * 1000);
     }
 }
