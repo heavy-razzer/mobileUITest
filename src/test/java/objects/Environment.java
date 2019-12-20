d6cff3f1131aa8f35510056fa52473467c60b876
Michael Miroshnichenko <hikari.no.mikem@gmail.com>
2019-12-20 16:51:46 +0200

    Fix getters

--- src/test/java/objects/Environment.java
+++ src/test/java/objects/Environment.java
@@ -5,6 +5,6 @@ Run environments list
  */
 public class Environment {
 
-    public static String local = MSG.ENV_LOCAL.toString();
-    public static String browserStack = MSG.ENV_BS.toString();
+    public static String local = MSG.ENV_LOCAL.getMessageText();
+    public static String browserStack = MSG.ENV_BS.getMessageText();
 }
